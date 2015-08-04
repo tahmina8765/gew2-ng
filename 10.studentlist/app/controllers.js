2f38e0ec0c5418973a70fc8f7c5a2ab15c81f860
Tahmina Khatoon <tahmina8765@yahoo.com>
2015-08-04 15:05:44 +0600

    studentadd

--- 10.studentlist/app/controllers.js
+++ 10.studentlist/app/controllers.js
@@ -1,48 +1,63 @@
 (function () {
     var app = angular.module('StudentApp');
     function StudentsController($scope, $log) {
+        $scope.newStudent = {
+            'name': ''
+            , 'email': ''
+            , 'phone': ''
+        };
         $scope.students = [
             {
                 'name': 'Raj Kapur Saha'
-                ,'email': 'kapur.raj@genweb2.com'
-                ,'phone': '8801717094921'
+                , 'email': 'kapur.raj@genweb2.com'
+                , 'phone': '8801717094921'
             }
-            ,{
+            , {
                 'name': 'Ahad Ahmed'
-                ,'email': 'ahmed.ahad@genweb2.com'
-                ,'phone': '8801710451477'
+                , 'email': 'ahmed.ahad@genweb2.com'
+                , 'phone': '8801710451477'
             }
-            ,{
+            , {
                 'name': 'Mohammad Golam Rabbi'
-                ,'email': 'rabbi.golam@genweb2.com'
-                ,'phone': '8801719359331'
+                , 'email': 'rabbi.golam@genweb2.com'
+                , 'phone': '8801719359331'
             }
-            ,{
+            , {
                 'name': 'Dola Chakraborty'
-                ,'email': 'chakraborty.dola@genweb2.com'
-                ,'phone': '8801823258558'
+                , 'email': 'chakraborty.dola@genweb2.com'
+                , 'phone': '8801823258558'
             }
-            ,{
+            , {
                 'name': 'Mahmudul Hassan'
-                ,'email': 'hassan.mahmudul@genweb2.com'
-                ,'phone': '8801675729214'
+                , 'email': 'hassan.mahmudul@genweb2.com'
+                , 'phone': '8801675729214'
             }
-            ,{
+            , {
                 'name': 'Invalid Student'
-                ,'email': ''
-                ,'phone': ''
+                , 'email': ''
+                , 'phone': ''
             }
-            
+
         ];
-        
-        $scope.numLimit = 50;        
-        $scope.displayEmail = function(student){
-            if(student.email != '' && student.email.length > 0){
+
+        $scope.numLimit = 50;
+
+        $scope.displayEmail = function (student) {
+            if (student.email != '' && student.email.length > 0) {
                 alert(student.email);
-            }else{
+            } else {
                 $log.warn('Email is not available')
             }
-            
+
+        };
+        $scope.saveStudent = function (student) {
+            console.log(student);
+            $scope.students.push(student);
+//            $scope.newStudent = {
+//                'name': ''
+//                , 'email': ''
+//                , 'phone': ''
+//            };
         };
     }
     ;
