9c4230cabfa6974a9fe9f3e29155331d3e1ae92a
Tahmina Khatoon <tahmina8765@yahoo.com>
2015-08-11 12:53:59 +0600

    Add watcher, unbindWatcher, watchCollection, Apply function

---
+++ 12. UnbindWatchers/app/controllers.js
@@ -0,0 +1,77 @@
+(function () {
+    var app = angular.module('StudentApp');
+    function StudentsController($scope, $log) {
+        $scope.newStudent = {
+            'name': ''
+            , 'email': ''
+            , 'phone': ''
+        };
+        $scope.students = [
+            {
+                'name': 'Raj Kapur Saha'
+                , 'email': 'kapur.raj@genweb2.com'
+                , 'phone': '8801717094921'
+            }
+            , {
+                'name': 'Ahad Ahmed'
+                , 'email': 'ahmed.ahad@genweb2.com'
+                , 'phone': '8801710451477'
+            }
+            , {
+                'name': 'Mohammad Golam Rabbi'
+                , 'email': 'rabbi.golam@genweb2.com'
+                , 'phone': '8801719359331'
+            }
+            , {
+                'name': 'Dola Chakraborty'
+                , 'email': 'chakraborty.dola@genweb2.com'
+                , 'phone': '8801823258558'
+            }
+            , {
+                'name': 'Mahmudul Hassan'
+                , 'email': 'hassan.mahmudul@genweb2.com'
+                , 'phone': '8801675729214'
+            }
+            , {
+                'name': 'Invalid Student'
+                , 'email': ''
+                , 'phone': ''
+            }
+
+        ];
+
+        $scope.numLimit = 50;
+        $scope.displayEmailCount = 0;
+
+        $scope.displayEmail = function (student) {
+            if (student.email != '' && student.email.length > 0) {
+                $scope.displayEmailCount++;
+                alert(student.email);
+            } else {
+                $log.warn('Email is not available')
+            }
+        };
+        
+        var unbindDisplayEmailWatcher = $scope.$watch('displayEmailCount', function (newValue, oldValue) {
+            console.log('called ' + newValue + ' times');
+            if (newValue == 2) {
+                alert('Point Deducted.');
+                $scope.displayEmailCount = 0;
+                unbindDisplayEmailWatcher();
+            }
+        });
+
+        $scope.saveStudent = function (student) {
+            console.log(student);
+            $scope.students.push(student);
+//            $scope.newStudent = {
+//                'name': ''
+//                , 'email': ''
+//                , 'phone': ''
+//            };
+        };
+    }
+    ;
+    app.controller("StudentsController", StudentsController);
+    StudentsController.$inject = ["$scope", "$log"];
+})();
---
+++ 14. Apply/app/controllers.js
@@ -0,0 +1,114 @@
+(function () {
+    var app = angular.module('StudentApp');
+    function StudentsController($scope, $log, $timeout) {
+        $scope.newStudent = {
+            'id': 0
+            , 'name': ''
+            , 'email': ''
+            , 'phone': ''
+        };
+
+        $scope.courseInfo = {
+            'name': 'Getting better with AngularJs',
+            'point': 500
+        };
+
+        $scope.students = [
+            {
+                'id': 1
+                , 'name': 'Raj Kapur Saha'
+                , 'email': 'kapur.raj@genweb2.com'
+                , 'phone': '8801717094921'
+            }
+            , {
+                'id': 2
+                , 'name': 'Ahad Ahmed'
+                , 'email': 'ahmed.ahad@genweb2.com'
+                , 'phone': '8801710451477'
+            }
+            , {
+                'id': 3
+                , 'name': 'Mohammad Golam Rabbi'
+                , 'email': 'rabbi.golam@genweb2.com'
+                , 'phone': '8801719359331'
+            }
+            , {
+                'id': 4
+                , 'name': 'Dola Chakraborty'
+                , 'email': 'chakraborty.dola@genweb2.com'
+                , 'phone': '8801823258558'
+            }
+            , {
+                'id': 5
+                , 'name': 'Mahmudul Hassan'
+                , 'email': 'hassan.mahmudul@genweb2.com'
+                , 'phone': '8801675729214'
+            }
+        ];
+
+        $scope.numLimit = 50;
+        $scope.displayEmailCount = 0;
+
+        $scope.displayEmail = function (student) {
+            if (student.email != '' && student.email.length > 0) {
+                $scope.displayEmailCount++;
+                alert(student.email);
+            } else {
+                $log.warn('Email is not available')
+            }
+        };
+
+        $scope.$watchCollection('courseInfo', function (newCollection, oldCollection) {
+            console.log(newCollection);
+            console.log(oldCollection);
+            if (newCollection.point != oldCollection.point) {
+                console.log('Collection has changed.');
+            }
+        });
+
+        $scope.saveStudent = function (student) {
+            var valid = true;
+            if (valid) {
+                student.id = $scope.students.length + 1;
+                $scope.students.push(student);
+                $scope.courseInfo.point = $scope.courseInfo.point + 100;
+                $scope.newStudent = {
+                    id: 0
+                    , 'name': ''
+                    , 'email': ''
+                    , 'phone': ''
+                };
+            }
+
+        };
+        var advertisements = [
+            'Advertisement of Google'
+                    , 'Advertisement of Prothom Alo'
+                    , 'Advertisement of Daily Star'
+                    , 'Advertisement of Times'
+                    , 'Advertisement of Genweb2'
+        ];
+
+        $scope.advertisement = '';
+
+//        setInterval(function () {
+//            $scope.$apply(function () {
+//                $scope.advertisement = advertisements[Math.floor(Math.random() * advertisements.length)];
+//                console.log('message=' + $scope.advertisement); //log this to console
+//            });
+//        }, 3000);
+
+        $timeout(function () {
+            $scope.advertisement = advertisements[Math.floor(Math.random() * advertisements.length)];
+            console.log('message=' + $scope.advertisement); //log this to console            
+        }, 3000);
+
+
+
+
+
+    }
+    ;
+    app.controller("StudentsController", StudentsController);
+    StudentsController.$inject = ["$scope", "$log", "$timeout"];
+})();
